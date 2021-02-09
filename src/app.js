b29c2f9593b74f6fafd72ca6ba2aa14825a8531e
circlehotarux <mightyherox@gmail.com>
2021-02-09 22:23:49 +0800

    add function getRealUrl

---
+++ src/app.js
@@ -0,0 +1,54 @@
+const { program } = require('commander');
+const url = require('url');
+const got = require('got');
+const cheerio = require('cheerio');
+
+async function getRealUrl(url) {
+    const videoURL = new URL(url);
+    const host = videoURL.hostname,
+        path = videoURL.pathname;
+    const header = {
+        'authority': host,
+        'method': 'GET',
+        'path': path,
+        'scheme': 'https',
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+        'accept-encoding': 'gzip, deflate, br',
+        'accept-language': 'zh-CN,zh;q=0.9',
+        'cache-control': 'max-age=0',
+        'upgrade-insecure-requests': '1',
+        'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.25 Mobile Safari/537.36',
+    };
+
+    let realUrl = '';
+
+    try {
+        const response = await got(url, { headers: header });
+
+        const $ = cheerio.load(response.body);
+        $('script').each((i, elem) => {
+            let content = $(elem).html();
+            if (content.indexOf('readyVideoUrl') != -1) {
+                const re = /readyVideoUrl: \'[\w\W]*\',/;
+                let search = re.exec(content)[0];
+                realUrl = search.slice('readyVideoUrl: \''.length, search.length - 2);
+                return false;
+            }
+        })
+    } catch (error) {
+        console.log(error.response.body);
+    }
+
+    return realUrl;
+}
+
+program
+    .version('0.0.1')
+    .description("get the real address of Bilibili video")
+    .option("-u, --url <url>", "the url of Bilibili video", "https://www.bilibili.com/video/BV1yv411x7KQ")
+    .action(async (option) => {
+        let realUrl = await getRealUrl(option.url);
+        console.log(realUrl);
+    });
+
+program.parse(process.argv);
